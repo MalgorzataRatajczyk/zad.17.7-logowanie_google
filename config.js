ccc79f05f76065b3d9c6082b481594ba5409eb99
Malgorzata Ratajczyk <malgorzataratajczyk@wp.pl>
2018-11-01 16:19:13 +0100

    Init project

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '674602715706-0upkjqni9pr29sprl7s7tku7hhh9vo3b.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '0Gvr_8eGjP0_B6YZgT1O4wWz',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
